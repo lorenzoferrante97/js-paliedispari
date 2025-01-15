b3938cb932a5e3a122be3b61a7aaaba8382b66e8
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-15 12:36:56 +0100

    modify: add function to es. 1

--- main.js
+++ main.js
@@ -13,13 +13,21 @@
 //      - se parola utente = parola invertita: la parola è palindroma
 //      - altrimenti: non è palindroma 
 
+function isPalindromo (p1, p2) {
+    if (p1 == p2) {
+        console.log("La parola è palindroma")
+    } else {
+        console.log("La parola NON è palindroma")
+    }
+}
+
 let parolaUtente = prompt("Inserisci una parola qualsiasi", "osso");
 console.log(`la parola inserita è: ${parolaUtente}`)
 
 let parolaInvertita = parolaUtente.split("").reverse().join("");
 console.log(`la parola invertita diventa: ${parolaInvertita}`)
 
-parolaInvertita == parolaUtente ? console.log("La parola è palindroma") : console.log("La parola NON è palindroma");
+isPalindromo(parolaUtente, parolaInvertita);
 
 
 // esercizio pari o dispari
