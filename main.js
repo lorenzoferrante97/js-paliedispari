50caf6e31923d25be4751c91f7f71f4811746ee7
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-15 12:28:50 +0100

    end: end es. 1

--- main.js
+++ main.js
@@ -13,7 +13,13 @@
 //      - se parola utente = parola invertita: la parola è palindroma
 //      - altrimenti: non è palindroma 
 
+let parolaUtente = prompt("Inserisci una parola qualsiasi", "osso");
+console.log(`la parola inserita è: ${parolaUtente}`)
 
+let parolaInvertita = parolaUtente.split("").reverse().join("");
+console.log(`la parola invertita diventa: ${parolaInvertita}`)
+
+parolaInvertita == parolaUtente ? console.log("La parola è palindroma") : console.log("La parola NON è palindroma");
 
 
 // esercizio pari o dispari
