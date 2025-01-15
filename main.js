baedcba408d89b9adf8c4aa98009039363fa7829
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-15 12:22:59 +0100

    add: es. 1 workflow

--- main.js
+++ main.js
@@ -2,7 +2,16 @@
 
 // esercizio palindromi
 
+// workflow
 
+// 1. chiedere all'utene una parola
+// 2. nuova var con parola invertita ( per controllo successivo)
+//      - trasformare input utente in array
+//      - inversione
+//      - trasformare in stringa
+// 3. controllo se la parola è palindroma
+//      - se parola utente = parola invertita: la parola è palindroma
+//      - altrimenti: non è palindroma 
 
 
 
